4c22a743ebf8a3aaf42248c62fd43ec0fa333f2c
noupsovan <noupsovan18@kit.edu.kh>
2022-09-17 00:18:36 +0700

    Add Origin

--- src/main/java/com/life_calendar/life_calendar/controller/api/NoteController.java
+++ src/main/java/com/life_calendar/life_calendar/controller/api/NoteController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/note/")
 @RequiredArgsConstructor
--- src/main/java/com/life_calendar/life_calendar/controller/api/UserController.java
+++ src/main/java/com/life_calendar/life_calendar/controller/api/UserController.java
@@ -19,6 +19,7 @@ import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/")
 @RequiredArgsConstructor
