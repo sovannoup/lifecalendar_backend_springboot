825f4b8343a5cf5db60dbe68a9f63e1865f11dc1
noupsovan <noupsovan18@kit.edu.kh>
2022-09-17 23:23:51 +0700

    Config Cors

--- src/main/java/com/life_calendar/life_calendar/security/CorsFilter.java
+++ src/main/java/com/life_calendar/life_calendar/security/CorsFilter.java
@@ -11,7 +11,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request= (HttpServletRequest) servletRequest;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
